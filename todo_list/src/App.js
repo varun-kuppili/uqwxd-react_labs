372697dcf840e98a81de8b0b24e74d8bea2127a3
Varun <kvvarun23@gmail.com>
2023-04-29 16:12:12 -0700

    added delete button and delete fn

--- todo_list/src/App.js
+++ todo_list/src/App.js
@@ -22,6 +22,12 @@ const App = () => {
         setTodo("");
     }
   }
+
+  function deleteTodo(id) {
+    let updatedTodos = [...todos].filter((todo) => todo.id !== id);
+    setTodos(updatedTodos);
+  }
+
   return (
       <div id="todo-list">
         <h1>Todo List</h1>
@@ -37,6 +43,7 @@ const App = () => {
         {todos.map((todo) => <div className="todo" key={todo.id}>
             <div>{todo.text}</div>
 
+            <button onClick={() => deleteTodo(todo.id)}>Delete</button>
         </div>)}
         </div>
 
